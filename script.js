5a440b8f68f6d30783fae92f608a6dd65a759728
Ismail <218119368+ismailwebdev68@users.noreply.github.com>
2025-07-17 19:41:35 +0500

    Made the calculator with UI

---
+++ script.js
@@ -0,0 +1,18 @@
+let n1 = 0
+let n2 = 0
+
+function add(a,b){
+    return a + b
+}
+function subtract(a,b){
+    return a + b
+}
+function multiply(a,b){
+    return a + b
+}
+function divide(a,b){
+    return a + b
+}
+function operate(a,b,op){
+
+}
